a634991ea3e287338b152335d6dc7dd13d16d288
1ien4nh <anhnt@goads.vn>
2025-02-19 14:36:37 +0700

    add language role

--- app/Domains/Role/Controller/router.php
+++ app/Domains/Role/Controller/router.php
@@ -9,8 +9,8 @@ use Illuminate\Support\Facades\Route;
 Route::group(['middleware' => ['user-auth']], static function () {
     Route::get('/role', Index::class)->name('role.index');
     Route::any('/role/create', Create::class)->name('role.create');
-    Route::any('/role/{id}', Update::class)->name('role.update');
-    Route::any('/role/{id}/role-notification', UpdateRoleNotification::class)->name('role.update.role-notification');
-    Route::any('/role/{id}/vehicle', UpdateVehicle::class)->name('role.update.vehicle');
-    Route::any('/role/{id}/boolean/{column}', UpdateRoleBoolean::class)->name('role.update.boolean');
+    // Route::any('/role/{id}', Update::class)->name('role.update');
+    // Route::any('/role/{id}/role-notification', UpdateRoleNotification::class)->name('role.update.role-notification');
+    // Route::any('/role/{id}/vehicle', UpdateVehicle::class)->name('role.update.vehicle');
+    // Route::any('/role/{id}/boolean/{column}', UpdateRoleBoolean::class)->name('role.update.boolean');
 });
